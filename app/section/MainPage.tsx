2aa3bd8c4f2d07fe98af7a093d9ef55075269f1a
SafdarAliShah <programmersafdar@live.com>
2024-10-16 09:40:25 -0700

    Application: Start Frontend

---
+++ app/section/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoElement from "./TodoElement";
+
+const MainPage = () => {
+  return (
+    <div>
+      <div className="bg-[]">
+        <TodoElement />
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
